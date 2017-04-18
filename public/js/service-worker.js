16df1cb9f5f737ba7395634970f6255220d7d2ca
Oscar Duran <xoscarduranx@gmail.com>
2017-04-18 21:48:17 +0200

    add the service-worker

---
+++ public/js/service-worker.js
@@ -0,0 +1,39 @@
+var version = 'v1::'
+
+var offlineFundamentals = [
+  '',
+  '/',
+  '/css/all-landing.css',
+  '/js/app-landing.js'
+];
+
+var offLinePage = '<html><head><title>Opps we are offline!</title></head><body>Opps you are offline</body></html>'
+
+
+self.addEventListener('install',function(event) {
+  console.log('Installing...')
+
+  event.waitUntil(
+    caches.open(version + 'fundamentals').then(function (cache) {
+      return cache.addAll(offlineFundamentals);
+    }).catch(function () {
+    console.log('opppsss');
+    })
+  )
+})
+
+self.addEventListener('activate',function(event) {
+  console.log('Activated service worker')
+})
+
+self.addEventListener('fetch',function(event) {
+  console.log('fetch service');
+  console.log(event.request.url);
+  const  url = new URL(event.request.url);
+  console.log('fetch service worker')
+
+  if(event.request.url === 'http://localhost:8081/img/arrow1.png') {
+    console.log('Prova!')
+
+  }
+})
--- resources/assets/js/app-landing.js
+++ resources/assets/js/app-landing.js
@@ -23,3 +23,10 @@ const app = new Vue({
   el: '#app'
 })
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function () {
+        console.log('Todo okey');
+    }).catch(function (err) {
+        console.log('Error')
+    })
+}
